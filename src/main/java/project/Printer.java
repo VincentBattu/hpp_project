420a05b423a5ba8c51ab59c3eb0584b9209db499
Pierre Vassal <pvassal.94@gmail.com>
2017-05-09 12:27:29 +0200

    Fonction pour écrire pas terminée (retour à la ligne ne marche pas)

--- src/main/java/project/Printer.java
+++ src/main/java/project/Printer.java
@@ -2,9 +2,12 @@ package project;
 
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Printer implements Runnable {
@@ -20,28 +23,51 @@ public class Printer implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		
-		   File fichier =  new File("tmp/integers.bin") ;
-		   OutputStream os = null ;
-
-		    try {
-		       // ouverture d'un flux de sortie sur un fichier
-		      os =  new FileOutputStream(fichier);
-		      DataOutputStream dos =  new DataOutputStream(os) ; 
-
-		       for (int i : Arrays.asList(1,  2,  3,  4,  5)) {
-		         dos.writeInt(i) ;
-		      }
-
-		      }  catch (IOException e) {
-		          // gestion de l'erreur
-		    	  
-		      }  finally {
-		          // fermeture du flux
-		    	  os.close();
-		      }
+		File file =  new File("result.txt") ;
+		OutputStream os = null;
+		
+		try {
+			 os = new FileOutputStream(file);
+			DataOutputStream dos =  new DataOutputStream(os) ; 
+			
+			while(!bufferQueue.isEmpty()){
+				try {
+					dos.writeChars(bufferQueue.take());
+					dos.writeChars("\n");
+					System.out.println("WRITTEN");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				os.close();
+				System.out.println("FIN");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
+	public static void main(String[] args) {
+	
+		BlockingQueue<String> bufferQueue = new ArrayBlockingQueue<>(5);
+		bufferQueue.add("Coucou AZERTY");
+		bufferQueue.add("LaRobeDeDELPHINE");
+		
+		Printer pr = new Printer(bufferQueue);
+		Thread t = new Thread(pr);
+		t.start();
+	}
 	
 
 }
