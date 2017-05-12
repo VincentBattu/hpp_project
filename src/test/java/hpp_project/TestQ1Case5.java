3f619719530e6797e2a5aa3f2d007f6b3afb5b73
Boucard Delphine <delphine96.boucard@orange.fr>
2017-05-12 09:53:30 +0200

    Création Q1Case5 (passe pas pr le moment)

--- src/test/java/hpp_project/TestQ1Case5.java
+++ src/test/java/hpp_project/TestQ1Case5.java
@@ -11,14 +11,7 @@ import org.junit.Test;
 
 import project.Manager;
 
-
-/*
- * Test abhérrant !!
- * Commentaire créé à la milliseconde près du post
- * donc on veut l'ajouter au post avant que ce dernier soit créer
- */
-
-public class TestQ1Case4 {
+public class TestQ1Case5 {
 
 	@SuppressWarnings("resource")
 	@Test
@@ -26,18 +19,18 @@ public class TestQ1Case4 {
 		BufferedReader buff = null;
 		BufferedReader buff1 = null;
 
-		new Manager("data/Tests/Q1Case4/posts.dat", "data/Tests/Q1Case4" + "/comments.dat",
-				"data/Tests/Q1Case4/result.txt");
+		new Manager("data/Tests/Q1Case5/posts.dat", "data/Tests/Q1Case5" + "/comments.dat",
+				"data/Tests/Q1Case5/result.txt");
 
 		try {
-			buff = new BufferedReader(new FileReader("data/Tests/Q1Case4/_expectedQ1.txt"));
+			buff = new BufferedReader(new FileReader("data/Tests/Q1Case5/_expectedQ1.txt"));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		try {
-			buff1 = new BufferedReader(new FileReader("data/Tests/Q1Case4/result.txt"));
+			buff1 = new BufferedReader(new FileReader("data/Tests/Q1Case5/result.txt"));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -65,4 +58,5 @@ public class TestQ1Case4 {
 			}
 		}
 	}
+
 }
